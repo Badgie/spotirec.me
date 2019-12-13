4bca75f38587f6950207ac859c82c5d9231e2827
Badgy <jonaskhansen@gmail.com>
2019-12-13 13:29:13 +0100

    update structure

--- auth.py
+++ auth.py
@@ -6,13 +6,13 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('code', type=str, help='oauth code')
 args = parser.parse_args()
+values = ['yeet', args.code, 'sklfdjg', 3600, 392742, 'bearer', 'scopes']
 
 
 def update_db():
-    connection = sqlite3.connect('/home/spot/spotirec.db')
+    connection = sqlite3.connect('/var/www/spotirec.me/db/spotirec.db')
     c = connection.cursor()
-    c.execute(f'INSERT INTO spotirec VALUES ({os.environ["QUERY_STRING"]}, {args.code}, tokenkjsan, 3600, 382948, '
-              f'bearer, scopes)')
+    c.execute('INSERT INTO spotirec VALUES (?,?,?,?,?,?,?)', values)
     connection.commit()
     connection.close()
 
