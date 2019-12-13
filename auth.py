1ae2f0e9040a534635fca490e5396351b84e4ed8
Badgy <jonaskhansen@gmail.com>
2019-12-13 14:07:08 +0100

    revert to py3.7

--- auth.py
+++ auth.py
@@ -21,7 +21,7 @@ URL_BASE = 'https://api.spotify.com/v1'
 OAUTH_TOKEN_URL = 'https://accounts.spotify.com/api/token'
 
 
-def error_handle(request_domain, expected_code, request_type, response):
+def error_handle(request_domain: str, expected_code: int, request_type: str, response=None):
     """
     Dispatch error message depending on request type
     :param request_domain: domain of the request, e.g. 'recommendation'
@@ -38,18 +38,18 @@ def error_handle(request_domain, expected_code, request_type, response):
         exit(1)
 
 
-def get_user_id(headers) -> str:
+def get_user_id(headers: dict) -> str:
     """
     Retrieve user ID from API.
     :param headers: request headers
     :return: user ID as a string
     """
     response = requests.get(f'{URL_BASE}/me', headers=headers)
-    error_handle('user info', 200, 'GET', response)
+    error_handle('user info', 200, 'GET', response=response)
     return json.loads(response.content.decode('utf-8'))['id']
 
 
-def retrieve_access_token(code) -> json:
+def retrieve_access_token(code: str) -> json:
     """
     Request token from API, save to cache, and return it.
     :param code: authorization code retrieved from spotify API
@@ -59,7 +59,7 @@ def retrieve_access_token(code) -> json:
             'code': code,
             'redirect_uri': REDIRECT}
     response = requests.post(OAUTH_TOKEN_URL, data=body, headers=encode_header())
-    error_handle('token retrieve', 200, 'POST', response)
+    error_handle('token retrieve', 200, 'POST', response=response)
     return json.loads(response.content.decode('utf-8'))
 
 
