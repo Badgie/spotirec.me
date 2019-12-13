e225ffce51e16129067584f33988ea390611b669
Badgy <jonaskhansen@gmail.com>
2019-12-13 13:54:42 +0100

    update auth

--- auth.php
+++ auth.php
@@ -1,5 +1,5 @@
 <?php
-$s = escapeshellcmd('python /var/www/spotirec.me/cgi-bin/auth.py ' . escapeshellarg($_GET['code'])) . ' 2>&1';
+$s = escapeshellcmd('python /var/www/spotirec.me/cgi-bin/auth.py --auth ' . escapeshellarg($_GET['code'])) . ' 2>&1';
 echo $s;
 $output = shell_exec($s);
 echo $output;
