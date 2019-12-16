75a3330c0fa99cf4cdac79e452e2348b234a3d3f
Badgy <jonaskhansen@gmail.com>
2019-12-16 16:53:55 +0100

    revamp to flask

--- flaskr/conf.py
+++ flaskr/conf.py
@@ -1,6 +1,6 @@
 class Config:
-    CLIENT_ID = '78ba9bb95f5b40a2b74bc7733ea3cd81'
-    CLIENT_SECRET = 'a0189d34e14d44448ddd9ed71d46e6f7'
-    REDIRECT = 'http://localhost:8080/auth/token'
+    CLIENT_ID = ''
+    CLIENT_SECRET = ''
+    REDIRECT = ''
     SCOPES = 'user-top-read playlist-modify-public playlist-modify-private user-read-private user-read-email ' \
              'ugc-image-upload user-read-playback-state user-modify-playback-state user-library-modify'
